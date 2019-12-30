bf93c787c0a1ffd1dd5f7b2b8084bdcd647284d5
dkuji <daisuke.kuji@gmail.com>
2019-12-30 21:15:46 +0900

    Add skills page

--- portfolio/src/components/skill.vue
+++ portfolio/src/components/skill.vue
@@ -1,9 +1,154 @@
 <template>
-  <h1>skill</h1>
+  <div>
+    <v-container>
+      <v-layout row wrap>
+        <v-flex xs12 sm6 md6 v-for="item in infra" :key="item.title">
+          <v-card class="ma-3">
+            <v-card-title>{{ item.title }}</v-card-title>
+            <v-card-text>
+              <p>{{ item.text }}</p>
+            </v-card-text>
+            <v-simple-table>
+              <thead>
+                <tr>
+                  <th class="text-left">Technology</th>
+                  <th class="text-left">notes</th>
+                </tr>
+              </thead>
+              <tbody v-for="item2 in item.carrers" :key="item2.tech">
+                <tr>
+                  <td>{{ item2.tech }}</td>
+                  <td>{{ item2.notes }}</td>
+                </tr>
+              </tbody>
+            </v-simple-table>
+          </v-card>
+          <v-spacer></v-spacer>
+        </v-flex>
+
+        <!--
+        <v-flex xs12 md6>
+          <v-card title="title" class="mx-auto">
+            <v-card-title>Infrastructure</v-card-title>
+            <v-card-text>
+              <p>Infrastructure developer.</p>
+            </v-card-text>
+            <v-simple-table>
+              <thead>
+                <tr>
+                  <th class="text-left">Technology</th>
+                  <th class="text-left">Years</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr v-for="item in carrers" :key="item.tech">
+                  <td>{{ item.tech }}</td>
+                  <td>{{ item.year }}</td>
+                </tr>
+              </tbody>
+            </v-simple-table>
+          </v-card>
+        </v-flex>
+        -->
+      </v-layout>
+    </v-container>
+  </div>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      infra: [
+        {
+          title: "Infrastructure",
+          text:
+            "I like building a cloud-native infrastructure using a public cloud",
+          carrers: [
+            {
+              tech: "AWS",
+              notes: ""
+            },
+            {
+              tech: "GCP",
+              notes: ""
+            },
+            {
+              tech: "VMware",
+              notes: ""
+            },
+            {
+              tech: "OnPremises",
+              notes: "NEC, FUJITSU, HPE"
+            },
+            {
+              tech: "Kubernetes",
+              notes: "use GKE"
+            }
+          ]
+        },
+        {
+          title: "Dev Tools",
+          text: "I like Infrastructure as Code, modern tools.",
+          carrers: [
+            {
+              tech: "Github",
+              notes: ""
+            },
+            {
+              tech: "Ansible",
+              notes: ""
+            },
+            {
+              tech: "Terraform",
+              notes: ""
+            },
+            {
+              tech: "Circle CI",
+              notes: ""
+            },
+            {
+              tech: "Prometheus",
+              notes: ""
+            }
+          ]
+        },
+        {
+          title: "frontend",
+          text: "I want to challenge if I have the opportunity.",
+          carrers: [
+            {
+              tech: "Vue.js",
+              notes: "Maked this portfolio."
+            }
+          ]
+        },
+        {
+          title: "Backend",
+          text: "I want to challenge if I have the opportunity.",
+          carrers: [
+            {
+              tech: "Ruby",
+              notes: ""
+            },
+            {
+              tech: "Python",
+              notes: ""
+            },
+            {
+              tech: "django",
+              notes: "studying"
+            },
+            {
+              tech: "Ruby On Rails",
+              notes: "A little"
+            }
+          ]
+        }
+      ]
+    };
+  }
+};
 </script>
 
 <style>
